60a467962f0063d0c7637b09335edcf0717b8dde
Dan Boehmke <Daniel@webappclouds.com>
2019-10-23 17:08:11 -0400

    Firebase initialization

--- app/src/main/java/com/webappclouds/firebasedemoapp/MainActivity.kt
+++ app/src/main/java/com/webappclouds/firebasedemoapp/MainActivity.kt
@@ -2,11 +2,20 @@ package com.webappclouds.firebasedemoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import com.google.firebase.analytics.FirebaseAnalytics
+
 
 class MainActivity : AppCompatActivity() {
+    private var mFirebaseAnalytics: FirebaseAnalytics? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this)
     }
 }
