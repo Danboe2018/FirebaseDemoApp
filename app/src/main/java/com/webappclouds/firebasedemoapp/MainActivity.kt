4a4d9a44f55c0bf98486681e97614f40b0e3e450
Dan Boehmke <Daniel@webappclouds.com>
2019-10-24 11:40:04 -0400

    Adds login screen

--- app/src/main/java/com/webappclouds/firebasedemoapp/MainActivity.kt
+++ app/src/main/java/com/webappclouds/firebasedemoapp/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.webappclouds.firebasedemoapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
     private var mFirebaseAnalytics: FirebaseAnalytics? = null
+    private var mAuth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +21,34 @@ class MainActivity : AppCompatActivity() {
 
         // Obtain the FirebaseAnalytics instance.
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+        mAuth = FirebaseAuth.getInstance()
+    }
+
+    fun butLoginEvent(view: View) {
+        val email = etEmail.text.toString()
+        val password = etPassword.text.toString()
+        loginToFirebase(email, password)
+    }
+
+    fun loginToFirebase(email: String, password: String) {
+        mAuth!!.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(applicationContext, "Successful Login", Toast.LENGTH_LONG).show()
+                    val currentUser = mAuth!!.currentUser
+                    Log.d("Login", currentUser!!.uid)
+                } else {
+                    Toast.makeText(applicationContext, "Login Failed", Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = mAuth!!.currentUser
+        if (currentUser != null) {
+            val intent = Intent(this, Control::class.java)
+            startActivity(intent)
+        }
     }
 }
